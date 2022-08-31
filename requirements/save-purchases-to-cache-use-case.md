6510bfa6be075de6b1ade3cf7da2abfaeaeae591
Daniel Valente <danieelvaraujo@hotmail.com>
2022-08-31 18:36:39 -0300

    ensure LocalSavePurchases resolves sucessfully

--- requirements/save-purchases-to-cache-use-case.md
+++ requirements/save-purchases-to-cache-use-case.md
@@ -3,7 +3,7 @@
 > ## Caso de sucesso
 
 1. ✅ O sistema executa o comando "Salvar Compras"
-2. O sistema cria uma data para ser armazenada no cache
+2. ✅ O sistema cria uma data para ser armazenada no cache
 3. ✅ O sistema apaga os dados do cache atual
 4. ✅ O sistema grava os novos dados no cache
 5. O sistema não retorna nenhum erro
--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -36,7 +36,7 @@ describe("LocalSavePurchases", () => {
     const timestamp = new Date();
     const { sut, cacheStore } = makeSut(timestamp);
     const purchases = mockPurchases();
-    await sut.save(purchases);
+    const promise = sut.save(purchases);
 
     expect(cacheStore.messages).toEqual([
       CacheStoreSpy.Message.delete,
@@ -48,6 +48,7 @@ describe("LocalSavePurchases", () => {
       timestamp,
       value: purchases,
     });
+    await expect(promise).resolves.toBeFalsy();
   });
 
   test("Should throw if insert throws", async () => {
