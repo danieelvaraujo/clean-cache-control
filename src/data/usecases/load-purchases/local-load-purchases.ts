4e98bfd5fee4a6f7c73dcec2124e09d6ebdfcb58
Daniel Valente <danieelvaraujo@hotmail.com>
2022-08-31 20:24:51 -0300

    ensure LocalLoadPurchases returns a list of purchases if the cache is less than 3 days old

--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -17,9 +17,9 @@ export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
 
   async loadAll(): Promise<Array<LoadPurchases.Result>> {
     try {
-      this.cacheStore.fetch(this.key);
+      const cache = this.cacheStore.fetch(this.key);
 
-      return [];
+      return cache.value;
     } catch (error) {
       this.cacheStore.delete(this.key);
 
