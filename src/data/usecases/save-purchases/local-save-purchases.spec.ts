5838b58d5780e365e8971e7cce2fc825b24d1f0d
Daniel Valente <danieelvaraujo@hotmail.com>
2022-08-31 18:30:11 -0300

    merged tests

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -23,17 +23,6 @@ describe("LocalSavePurchases", () => {
     expect(cacheStore.messages).toEqual([]);
   });
 
-  test("Should delete old cache on sut.save", async () => {
-    const { sut, cacheStore } = makeSut();
-    await sut.save(mockPurchases());
-
-    expect(cacheStore.messages).toEqual([
-      CacheStoreSpy.Message.delete,
-      CacheStoreSpy.Message.insert,
-    ]);
-    expect(cacheStore.deleteKey).toBe("purchases");
-  });
-
   test("Should not insert new cache if delete fails", async () => {
     const { sut, cacheStore } = makeSut();
     cacheStore.simulateDeleteError();
@@ -52,6 +41,7 @@ describe("LocalSavePurchases", () => {
       CacheStoreSpy.Message.delete,
       CacheStoreSpy.Message.insert,
     ]);
+    expect(cacheStore.deleteKey).toBe("purchases");
     expect(cacheStore.insertKey).toBe("purchases");
     expect(cacheStore.insertValues).toEqual(purchases);
   });
